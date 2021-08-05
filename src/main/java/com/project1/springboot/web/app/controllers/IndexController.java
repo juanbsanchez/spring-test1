b1c7d8345e18784cbd0964d5e4c8b3c4b6a21575
juanbsanchez <juanbsanchez1988@gmail.com>
2021-08-05 18:28:25 +0200

    pass data to view from controller

--- src/main/java/com/project1/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/project1/springboot/web/app/controllers/IndexController.java
@@ -1,14 +1,16 @@
 package com.project1.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping({"/index", "/", "/home"})
-	public String index()
+	public String index(Model model)
 	{
+		model.addAttribute("title", "hi spring");
 		return "index";
 	}
 }
