55778bcfd1ac9d12fb1abc5effe95dbac5e10eef
juanbsanchez <juanbsanchez1988@gmail.com>
2021-08-05 18:00:36 +0200

    first commit

---
+++ src/test/java/com/project1/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
